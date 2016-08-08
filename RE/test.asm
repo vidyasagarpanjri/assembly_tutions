d03e20847d24b6fe801b55a513012cfca94e960f
unknown <vidyasagar.panjri@gmail.com>
2016-08-07 20:50:41 -0700

    change test.asm

--- RE/test.asm
+++ RE/test.asm
@@ -10,10 +10,15 @@
     include \masm32\include\masm32.inc
 .data
     msg db "vidya sagar panjri",0
+    len equ $ -msg
 .code
 start:
-    push offset msg
-    call StdOut
-    push 0
-    call ExitProcess
+    mov	edx,len     ;message length
+    mov	ecx,msg     ;message to write
+    mov	ebx,1       ;file descriptor (stdout)
+    mov	eax,4       ;system call number (sys_write)
+    int	0x80        ;call kernel
+	
+    mov	eax,1       ;system call number (sys_exit)
+    int	0x80        ;call kernel
 end start 
